d1a64f779c92739cf781c82e6c42ff0929444c29
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-18 15:32:18 +0100

    Add EF and datacontext class

---
+++ CreationApp/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using CreationApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreationApp
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+        }
+    }
+}
