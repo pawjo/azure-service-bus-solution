6d20b21796b4d60e203a06eab8fe8b9efdfd57a9
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-25 22:16:08 +0100

    Add Message and User models

---
+++ ValidationApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CreationApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
