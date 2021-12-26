16149551daaf9c0adc09b47bce1c1e239766b4ed
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-26 20:12:17 +0100

    Add ServiceBusWorker

---
+++ ValidationApp/Services/ServiceBusWorker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreationApp.Services
+{
+    public class ServiceBusWorker : IHostedService, IDisposable
+    {
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
