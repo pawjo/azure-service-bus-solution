dd5ea2c2d3303b1b40cbccc03bbf72368f1c03ef
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-26 21:05:55 +0100

    Added migration for fix user

--- CreationApp/Migrations/20211226195141_FixUserProperties.Designer.cs
+++ CreationApp/Migrations/20211226195141_FixUserProperties.Designer.cs
@@ -3,14 +3,16 @@ using CreationApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CreationApp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211226195141_FixUserProperties")]
+    partial class FixUserProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
