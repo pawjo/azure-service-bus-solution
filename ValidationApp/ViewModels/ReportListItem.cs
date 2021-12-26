6c4972dc01768bc59fe517a0753a8ea5f26d145c
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2021-12-26 21:28:51 +0100

    Add Reports view models in ValidationApp

---
+++ ValidationApp/ViewModels/ReportListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ValidationApp.ViewModels
+{
+    public class ReportListItem
+    {
+        public int Id { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Result { get; set; }
+    }
+}
